f2e79d3d9dca9e6a5415a091d5202b5874a674d9
Ingo Collatz <incollatz@mail.de>
2024-04-12 07:23:10 +0200

    no fixed navbar anymore

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,36 +1,34 @@
-<div class="fixed top-4 left-0 right-0 z-50 flex justify-center px-2">
-	<nav
-		class="flex justify-between items-center bg-secondary-bg px-5 py-2 shadow-lg rounded-full w-full max-w-screen-xl"
-	>
-		<div class="flex items-center">
-			<img src="/circles-logo-full-white.svg" alt="Logo" class="h-8 mr-4" />
-			<a
-				href="/"
-				class="text-lg text-white hover:text-gray-400 p-2 transition duration-300 ease-in-out rounded-md"
-				>Home</a
-			>
-			<a
-				href="/path-viewer"
-				class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
-				>Path Viewer</a
-			>
-			<a
-				href="/graph-explorer"
-				class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
-				>Graph Explorer</a
-			>
-		</div>
-		<div class="flex items-center">
-			<a
-				href="/qr"
-				class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
-				>QR</a
-			>
-			<a
-				href="/website"
-				class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
-				>Website</a
-			>
-		</div>
-	</nav>
-</div>
+<nav
+	class="flex justify-between items-center bg-secondary-bg px-5 py-2 shadow-lg rounded-full w-full max-w-screen-xl m-2"
+>
+	<div class="flex items-center">
+		<img src="/circles-logo-full-white.svg" alt="Logo" class="h-8 mr-4" />
+		<a
+			href="/"
+			class="text-lg text-white hover:text-gray-400 p-2 transition duration-300 ease-in-out rounded-md"
+			>Home</a
+		>
+		<a
+			href="/path-viewer"
+			class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
+			>Path Viewer</a
+		>
+		<a
+			href="/graph-explorer"
+			class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
+			>Graph Explorer</a
+		>
+	</div>
+	<div class="flex items-center">
+		<a
+			href="/qr"
+			class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
+			>QR</a
+		>
+		<a
+			href="/website"
+			class="text-lg p-2 text-white hover:text-gray-400 transition duration-300 ease-in-out rounded-md"
+			>Website</a
+		>
+	</div>
+</nav>
